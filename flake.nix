29632f64f8a209d122c410dc901dec2f6c4f32b2
Jack Maloney <jack@maloney.ai>
2023-01-05 16:22:26 -0600

    initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {self, nixpkgs, flake-utils}:
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in
+  flake-utils.lib.eachDefaultSystem (system: 
+    {
+      defaultPackage = pkgs.dockerTools.buildLayeredImage {
+        name = "git-annex";
+        contents = [
+          pkgs.git-annex
+          pkgs.iana-etc
+        ];
+      };
+    }
+  );
+}
