f2c0765b70b21ebece59eacb66e314242e411048
Jack Maloney <jack@maloney.ai>
2023-02-25 00:18:37 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,35 @@
   let
     pkgs = import nixpkgs { system = "x86_64-linux"; };
   in
-  flake-utils.lib.eachDefaultSystem (system: 
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    nonRootShadowSetup = { user, uid, gid ? uid }: with pkgs; [
+        (
+        writeTextDir "etc/shadow" ''
+          root:!x:::::::
+          ${user}:!:::::::
+        ''
+        )
+        (
+        writeTextDir "etc/passwd" ''
+          root:x:0:0::/root:${runtimeShell}
+          ${user}:x:${toString uid}:${toString gid}::/home/${user}:
+        ''
+        )
+        (
+        writeTextDir "etc/group" ''
+          root:x:0:
+          ${user}:x:${toString gid}:
+        ''
+        )
+        (
+        writeTextDir "etc/gshadow" ''
+          root:x::
+          ${user}:x::
+        ''
+        )
+      ];
+    in
     {
       defaultPackage = pkgs.dockerTools.buildLayeredImage {
         name = "git-annex";
@@ -21,10 +49,7 @@
           openssh
           git
           git-annex
-        ];
-        config.Env = [
-          "USER=nobody"
-        ];
+        ] ++ nonRootShadowSetup { uid = 999; user = "git"; };
       };
     }
   );
